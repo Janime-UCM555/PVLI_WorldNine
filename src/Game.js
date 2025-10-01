bd6f897d8db31b06ae019a19e86215ac7cf6e3d0
Janime555 <157594411+Janime-UCM555@users.noreply.github.com>
2025-10-01 16:29:56 +0200

    Primera version de la pagina

---
+++ src/Game.js
@@ -0,0 +1,24 @@
+// /**
+//  * Inicio del juego en Phaser. Creamos el archivo de configuración del juego y creamos
+//  * la clase Game de Phaser, encargada de crear e iniciar el juego.
+//  */
+// let config = {
+// 	type: Phaser.AUTO,
+// 	width:  800,
+// 	height: 600,
+// 	pixelArt: true,
+// 	parent: 'game',
+// 	scale: {
+// 		autoCenter: Phaser.Scale.CENTER_HORIZONTALLY
+// 	},
+// 	scene:[],	// Decimos a Phaser cual es nuestra escena
+// 	physics: { 
+// 		default: 'arcade', 
+// 		arcade: { 
+// 			gravity: { y: 0}, 
+// 			debug: true 
+// 		} 
+// 	},
+// };
+
+// new Phaser.Game(config);
