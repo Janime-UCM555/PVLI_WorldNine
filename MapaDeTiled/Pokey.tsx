22f6d7c5e6dcf806564578d807482d784e4ac874
DarkSideOficial <rcena@ucm.es>
2025-11-17 21:52:51 +0100

    Update Pokey.tsx

--- MapaDeTiled/Pokey.tsx
+++ MapaDeTiled/Pokey.tsx
@@ -1,4 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="Pokey" tilewidth="30" tileheight="30" tilecount="2" columns="2">
  <image source="../assets/GameSprites/Characters/Enemigos/Pokey/Pokey.png" width="60" height="30"/>
+ <tile id="1">
+  <properties>
+   <property name="segments" type="int" value="5"/>
+   <property name="speed" type="float" value="0.5"/>
+  </properties>
+ </tile>
 </tileset>
