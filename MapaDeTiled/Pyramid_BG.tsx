df7eddea061a5b394045fdf957ac00ac9921cc9b
DarkSideOficial <rcena@ucm.es>
2025-11-24 21:48:11 +0100

    Los tenía en otra branch XD

---
+++ MapaDeTiled/Pyramid_BG.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Pyramid_BG" tilewidth="32" tileheight="32" tilecount="3196" columns="94">
+ <image source="../assets/GameSprites/Tilesets/Pyramid_BG.png" width="3008" height="1088"/>
+</tileset>
