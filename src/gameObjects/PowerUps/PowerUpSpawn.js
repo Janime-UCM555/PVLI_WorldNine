424495b9ea94108782845681ebb482e47095d632
“GuillermoY” <64642169+GuillermoY@users.noreply.github.com>
2025-12-05 17:51:46 +0100

    PowerUpSpawn

--- src/gameObjects/PowerUps/PowerUpSpawn.js
+++ src/gameObjects/PowerUps/PowerUpSpawn.js
@@ -2,10 +2,27 @@ import PowerUp from "./PowerUps.js";
 // Spawner simple (tu PowerUp ya añade físicas y movimiento)
 export const spawnPowerUp= (scene, x, y, type) =>
 {
-    let power = new PowerUp(scene, x, y, type, type, 0)
+    let power;
+    switch (type) {
+    case POWERUP_TYPES.STAR:
+        power = new Star(scene, x, y);
+        break;
+    case POWERUP_TYPES.HAMMER:
+        power = new Hammer(scene, x, y);
+        break;
+    case POWERUP_TYPES.DOUBLE_JUMP:
+        power = new DoubleJump(scene, x, y);
+        break;
+    case POWERUP_TYPES.JUMP_BOOTS:
+        power = new JumpBoots(scene, x, y);
+        break;
+        case POWERUP_TYPES.MUSHROOM:
+        power = new Mushroom(scene, x, y);
+        break;
+    }
     power.setVelocityX(power.body.velocity.x * 0.09315); // Salir del bloque hacia arriba
     power.setVelocityY(-power.body.velocity.x/2);
-    scene.powerups.add(power);
-    return scene.powerups;
+    this.scene.powerups.add(power);
+    return this.scene.powerups;
 }
 export default spawnPowerUp;
