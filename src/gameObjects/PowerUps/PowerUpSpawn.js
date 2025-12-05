a9bce12a9e93a0d5d2cc9bc5feca7b4356b77f64
Janime555 <157594411+Janime-UCM555@users.noreply.github.com>
2025-12-05 18:07:42 +0100

    Refactor spawnPowerUp and improve documentation
    Added detailed JSDoc comments to the spawnPowerUp function for better clarity. Refactored code for readability, fixed group addition to use the passed scene, and changed the return value to the spawned PowerUp instead of the group.

--- src/gameObjects/PowerUps/PowerUpSpawn.js
+++ src/gameObjects/PowerUps/PowerUpSpawn.js
@@ -2,32 +2,59 @@ import DoubleJump from "./DoubleJump.js";
 import Hammer from "./Hammer.js";
 import JumpBoots from "./HighJump.js";
 import Mushroom from "./Mushroom.js";
-import {POWERUP_TYPES } from "./PowerUps.js";
+import { POWERUP_TYPES } from "./PowerUps.js";
 import Star from "./Star.js";
-// Spawner simple (tu PowerUp ya añade físicas y movimiento)
-export const spawnPowerUp= (scene, x, y, type) =>
+
+/**
+ * Spawnea un nuevo PowerUp en la escena según su tipo.
+ * 
+ * @function spawnPowerUp
+ * @param {Phaser.Scene} scene - La escena donde se creará el PowerUp.
+ * @param {number} x - Coordenada X de aparición.
+ * @param {number} y - Coordenada Y de aparición.
+ * @param {POWERUP_TYPES} type - Tipo de PowerUp a generar.
+ * 
+ * @returns {Phaser.GameObjects.GameObject} El PowerUp generado.
+ * 
+ * @description
+ * Crea una instancia del PowerUp adecuado según el tipo recibido,
+ * aplica una velocidad inicial para que salga "expulsado" del bloque
+ * y lo añade al grupo `scene.powerups`.
+ */
+export const spawnPowerUp = (scene, x, y, type) =>
 {
     let power;
+
     switch (type) {
-    case POWERUP_TYPES.STAR:
-        power = new Star(scene, x, y);
-        break;
-    case POWERUP_TYPES.HAMMER:
-        power = new Hammer(scene, x, y);
-        break;
-    case POWERUP_TYPES.DOUBLE_JUMP:
-        power = new DoubleJump(scene, x, y);
-        break;
-    case POWERUP_TYPES.JUMP_BOOTS:
-        power = new JumpBoots(scene, x, y);
-        break;
+        case POWERUP_TYPES.STAR:
+            power = new Star(scene, x, y);
+            break;
+
+        case POWERUP_TYPES.HAMMER:
+            power = new Hammer(scene, x, y);
+            break;
+
+        case POWERUP_TYPES.DOUBLE_JUMP:
+            power = new DoubleJump(scene, x, y);
+            break;
+
+        case POWERUP_TYPES.JUMP_BOOTS:
+            power = new JumpBoots(scene, x, y);
+            break;
+
         case POWERUP_TYPES.MUSHROOM:
-        power = new Mushroom(scene, x, y);
-        break;
+            power = new Mushroom(scene, x, y);
+            break;
     }
-    power.setVelocityX(power.body.velocity.x * 0.09315); // Salir del bloque hacia arriba
-    power.setVelocityY(-power.body.velocity.x/2);
-    this.scene.powerups.add(power);
-    return this.scene.powerups;
-}
+
+    // Velocidades iniciales para simular que sale disparado del bloque
+    power.setVelocityX(power.body.velocity.x * 0.09315);
+    power.setVelocityY(-power.body.velocity.x / 2);
+
+    // Añadir al grupo de PowerUps de la escena
+    scene.powerups.add(power);
+
+    return power;
+};
+
 export default spawnPowerUp;
